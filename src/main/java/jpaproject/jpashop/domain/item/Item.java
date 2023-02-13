a45829b0c1f59db3880e6cb8b068c48784f95b33
sunghyeok <glxm06052@gmail.com>
2023-02-13 12:35:53 +0900

    addStock

--- src/main/java/jpaproject/jpashop/domain/item/Item.java
+++ src/main/java/jpaproject/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpaproject.jpashop.domain.item;
 
 import jpaproject.jpashop.domain.Category;
+import jpaproject.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,5 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
