565679c32a78684832468c530e5b1d948ecf461b
sunghyeok <glxm06052@gmail.com>
2023-03-13 18:27:44 +0900

    OrderService

--- src/main/java/jpaproject/jpashop/domain/OrderItem.java
+++ src/main/java/jpaproject/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpaproject.jpashop.domain;
 
 import jpaproject.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -27,5 +30,24 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; //상품수량
 
+    // 생성 메소드
+    public static OrderItem createOrderItem(Item item,int orderPrice,int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
