14d2caa52016caa96d36ad30fb324a3a91afb2a8
sunghyeok <glxm06052@gmail.com>
2023-04-10 14:17:45 +0900

    1

---
+++ src/main/java/jpaproject/jpashop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpaproject.jpashop.repository;
+
+import jpaproject.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
