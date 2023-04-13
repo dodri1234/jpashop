736715397e176d1e0ee85584558ca4e1fa8af85d
sunghyeok <glxm06052@gmail.com>
2023-04-13 19:32:52 +0900

    item list

--- src/main/java/jpaproject/jpashop/controller/ItemController.java
+++ src/main/java/jpaproject/jpashop/controller/ItemController.java
@@ -1,12 +1,15 @@
 package jpaproject.jpashop.controller;
 
 import jpaproject.jpashop.domain.item.Book;
+import jpaproject.jpashop.domain.item.Item;
 import jpaproject.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,5 +34,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String itemList(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items",items);
+        return "items/itemList";
     }
 }
